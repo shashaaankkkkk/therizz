d50de8113c588226cff74a7955a14da04bf1fa42
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2024-10-31 04:12:05 +0530

    login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
-const flowbite = require("flowbite-react/tailwind");
+// const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 
+import flowbite from "flowbite-react/tailwind";
+
 export default {
-  content: [
-    
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-      flowbite.content(),
-    
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [
-    flowbite.plugin(),
-  ],
-}
-
-
+  plugins: [flowbite.plugin()],
+};
