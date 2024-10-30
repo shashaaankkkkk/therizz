4c58908cbc03534e8f67d875c6b2819a1086e977
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-10-31 02:50:55 +0530

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Example } from './components/RetractingSideBar'
+import { Sidebar2 } from './components/RetractingSideBar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <div>
-    <Example></Example>
+    <Sidebar></Sidebar>
    </div>
   )
 }
