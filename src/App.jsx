bbab37290b96925818d596e47afb6f4c7a578cc4
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2024-10-31 04:58:26 +0530

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Sidebar2 } from './components/RetractingSideBar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Sidebar2 } from "./components/RetractingSideBar";
+import LoginPage from "./components/login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // Add state to control whether to show login or main content
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-   <div className='fixed left-0 top-0 h-full'>
-    <Sidebar2></Sidebar2>
-   </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Login page as home page */}
+        <Route path="/" element={<LoginPage />} />
+
+        {/* Main layout with sidebar at /dashboard */}
+        <Route
+          path="/dashboard"
+          element={
+            <div className="fixed left-0 top-0 h-full">
+              <Sidebar2 />
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
