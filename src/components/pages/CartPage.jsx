a15c09c87a52a5b14e767b8349bb072719177a18
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-11-04 03:45:19 +0530

    upd

--- src/components/pages/CartPage.jsx
+++ src/components/pages/CartPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import UserDashboardLayout from "../User/UserDashboardLayout"; // Adjust the path as necessary
 import CartTable2 from "../User/cart";
 import ExploreMoreItems from "../User/ExploreItems";
 // Import the CartTable component
