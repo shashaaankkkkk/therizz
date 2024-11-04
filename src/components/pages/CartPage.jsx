0c49cad2cef02c1779b6a078922e4095be0aa84e
Shashank Shekhar <shashankshekhar8534@gmail.com>
2024-11-05 05:12:11 +0530

    some changes

--- src/components/pages/CartPage.jsx
+++ src/components/pages/CartPage.jsx
@@ -1,12 +1,37 @@
 import React from "react";
-import CartTable2 from "../User/Cart";
+import CartTable from "../User/Cart";
 import ExploreMoreItems from "../User/ExploreItems";
+import icons from "../../utils/utils";
 // Import the CartTable component
 
 const CartPage = () => {
+  const cartItems = [
+    {
+      id: 1,
+      image: icons.image2,
+      product: "Classic Monochrome Tees",
+      quantity: 2,
+      price: 29.99,
+    },
+    {
+      id: 2,
+      image: icons.image,
+      product: "Sleek and Cozy Black",
+      quantity: 1,
+      price: 19.99,
+    },
+    {
+      id: 3,
+      image: icons.image6,
+      product: "Elegant Ebony Sweatshirts",
+      quantity: 1,
+      price: 49.99,
+    },
+    // Add more cart items here
+  ];
   return (
     <div>
-      <CartTable2 />
+      <CartTable cartItems={cartItems} />
       <ExploreMoreItems /> {/* Render the CartTable */}
     </div>
   );
