7d5e3ec9821dd2f0051a3537e3f70a88abc7a36d
Shashank Shekhar <shashankshekhar8534@gmail.com>
2024-11-05 16:40:50 +0530

    route changes to the errorpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// App.jsx
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -28,16 +27,14 @@ import UserSignup from "./components/User/Authentication/UserSignIn";
 import OrderSuccess from "./components/User/PaymentDone";
 import ForgotPassword from "./components/User/Authentication/UserForgotPassword";
 import ResetPassword from "./components/User/Authentication/ResetPassword";
-// import OrderFail from "./components/User/PaymentFail";
 import CustomerProfileLayout from "./components/User/custProfileLayout";
-// import OrderSimpleTableExample from "./components/User/CustOrders";
 import OrderHistoryTableContainer from "./components/User/CustOrders";
 import AddressPage from "./components/User/Address";
 import Wishlist from "./components/User/Wishlist";
 import AccountDetails from "./components/User/AccountDetails";
 import EmptyOrderHistory from "./components/User/EmptyOrders";
-
 import NotFoundPage from "./components/Error404";
+
 // Protected Route wrapper for admin routes
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
@@ -49,66 +46,61 @@ const ProtectedRoute = ({ children }) => {
 
 const App = () => {
   return (
-    <NotFoundPage/>
-    // <Router>
-    //   <Routes>
-    //     {/* Default route - redirects to login */}
-    //     <Route path="/" element={<Navigate to="/login" replace />} />
+    <Router>
+      <Routes>
+        {/* Default route - redirects to login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
 
-    //     {/* Auth Routes */}
-    //     <Route path="/login" element={<LoginPage />} />
-    //     <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-    //     <Route path="/reset-password" element={<ResetPasswordPage />} />
+        {/* Auth Routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
 
-    //     {/* Protected Admin Routes */}
-    //     <Route
-    //       path="/admin"
-    //       element={
-    //         <ProtectedRoute>
-    //           <DashboardLayout />
-    //         </ProtectedRoute>
-    //       }
-    //     >
-    //       <Route path="dashboard" element={<Dashboard />} />
-    //       <Route path="products" element={<ProductsPage />} />
-    //       <Route path="products/add" element={<AddProduct />} />
-    //       <Route path="orders" element={<OrderTable2 />} />
-    //       <Route path="customers" element={<CustomersPage />} />
-    //       <Route path="reviews" element={<ReviewsPage />} />
-    //       <Route path="settings" element={<Settings />} />
-    //     </Route>
+        {/* Protected Admin Routes */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="products" element={<ProductsPage />} />
+          <Route path="products/add" element={<AddProduct />} />
+          <Route path="orders" element={<OrderTable2 />} />
+          <Route path="customers" element={<CustomersPage />} />
+          <Route path="reviews" element={<ReviewsPage />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
 
-    //     {/* User Routes - All wrapped in UserDashboardLayout */}
-    //     <Route path="/user" element={<UserDashboardLayout />}>
-    //       <Route path="homepage" element={<HomePage />} />
-    //       <Route path="cart" element={<CartPage />} />
-    //       <Route path="product" element={<ProductDetail />} />
-    //       <Route path="listing" element={<Listing />} />
-    //       <Route path="checkout" element={<Checkout />} />
-    //       <Route path="ulogin" element={<UserLogin />} />
-    //       <Route path="usignin" element={<UserSignup />} />
-    //       <Route path="forgot password" element={<ForgotPassword />} />
-    //       <Route path="reset password" element={<ResetPassword />} />
-    //       <Route path="success" element={<OrderSuccess />} />
-    //     </Route>
+        {/* User Routes - All wrapped in UserDashboardLayout */}
+        <Route path="/user" element={<UserDashboardLayout />}>
+          <Route path="homepage" element={<HomePage />} />
+          <Route path="cart" element={<CartPage />} />
+          <Route path="product" element={<ProductDetail />} />
+          <Route path="listing" element={<Listing />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="ulogin" element={<UserLogin />} />
+          <Route path="usignin" element={<UserSignup />} />
+          <Route path="forgot password" element={<ForgotPassword />} />
+          <Route path="reset password" element={<ResetPassword />} />
+          <Route path="success" element={<OrderSuccess />} />
+        </Route>
 
-    //     {/* Profile vale sare system yaha se dalne hai */}
-       
-    //     <Route path="/profile" element={<CustomerProfileLayout />}>
-    //       <Route path="orders" element={<OrderHistoryTableContainer />} />
-    //       <Route path="address" element={<AddressPage />} />
-    //       <Route path="wishlist" element={<Wishlist />} />
-    //       <Route path="Your Details" element={<AccountDetails />} />
-    //       <Route path="empty orders" element={<EmptyOrderHistory/>} />
-          
-    //     </Route>
-          
-        
+        {/* Profile Routes */}
+        <Route path="/profile" element={<CustomerProfileLayout />}>
+          <Route path="orders" element={<OrderHistoryTableContainer />} />
+          <Route path="address" element={<AddressPage />} />
+          <Route path="wishlist" element={<Wishlist />} />
+          <Route path="Your Details" element={<AccountDetails />} />
+          <Route path="empty orders" element={<EmptyOrderHistory />} />
+        </Route>
 
-    //     {/* Catch all undefined routes - redirect to login */}
-    //     <Route path="*" element={<Navigate to="/login" replace />} />
-    //   </Routes>
-    // </Router>
+        {/* Catch all undefined routes - show NotFoundPage */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 };
 
